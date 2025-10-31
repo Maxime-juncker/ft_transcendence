ea636c5707a9dc775264e805ce1814e45609bb5f
Maxime Juncker <maxime.junck@gmail.com>
2025-10-31 08:53:01 +0100

    remove usless code

--- frontend/conf/entrypoint.sh
+++ frontend/conf/entrypoint.sh
@@ -1,22 +1,3 @@
-CERT_DIR="/etc/nginx/ssl"
-KEY_FILE="$CERT_DIR/nginx.key"
-CERT_FILE="$CERT_DIR/nginx.crt"
-SUBJ="/C=FR/ST=ge/L=CHARBONIERE/O=ME/CN=mjuncker.42.fr"
-
-mkdir -p "$CERT_DIR"
-
-if [ "$(ls -A $CERT_DIR)" ]; then
-	echo "key already generated"
-	exec nginx -g "daemon off;"
-fi
-
-echo "generating new ssl keys"
-openssl req -x509 -nodes -newkey rsa:2048 \
-	-keyout "$KEY_FILE" \
-	-out "$CERT_FILE" \
-	-days 365 \
-	-subj "$SUBJ"
-
 cd /var/www/html
 
 npm init -y
